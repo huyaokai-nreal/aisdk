1958ca057d3484dcdca03ae47caa9604b70a8ae9
rthao <rthao@nreal.ai>
2024-03-01 08:41:39 +0000

    [algorithm] add check_headpose_calculator

---
+++ aisdk/algorithm/calculator/check_headpose_calculator.cc
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <memory>
+
+#include "../internal_structs/headpose_struct_internal.h"
+#include "aisdk/base/log.h"
+#include "mediapipe/framework/calculator_framework.h"
+#include "mediapipe/framework/port/canonical_errors.h"
+
+// currently do nothing
+bool isHeadposeValid(const aisdk::algorithm::HeadPoseInternal& headpose) { return true; }
+
+namespace mediapipe {
+
+// A calculator check if headpose input valid.
+// Definition:
+// node {
+//   calculator: "CheckHeadposeCalculator"
+//   input_stream: "INPUT:head_pose"
+//   output_stream: "OUTPUT:head_pose_checked"
+// }
+
+class CheckHeadposeCalculator : public CalculatorBase {
+   private:
+   public:
+    static absl::Status GetContract(CalculatorContract* cc) {
+        AISDK_LOG_TRACE("[CheckHeadposeCalculator] GetContract start.");
+        cc->Inputs().Tag("INPUT").Set<aisdk::algorithm::HeadPoseInternal>();
+        cc->Outputs().Tag("OUTPUT").Set<aisdk::algorithm::HeadPoseInternal>();
+        AISDK_LOG_TRACE("[CheckHeadposeCalculator] GetContract complete.");
+        return absl::OkStatus();
+    }
+
+    absl::Status Open(CalculatorContext* cc) final {
+        AISDK_LOG_TRACE("[CheckHeadposeCalculator] Open start.");
+        AISDK_LOG_TRACE("[CheckHeadposeCalculator] Open complete.");
+        return absl::OkStatus();
+    }
+
+    absl::Status Process(CalculatorContext* cc) final {
+        AISDK_LOG_TRACE("[CheckHeadposeCalculator] Process start.");
+        const auto& input_data = cc->Inputs().Tag("INPUT").Get<aisdk::algorithm::HeadPoseInternal>();
+        std::unique_ptr<aisdk::algorithm::HeadPoseInternal> output_buffer_ =
+            absl::make_unique<aisdk::algorithm::HeadPoseInternal>();
+        output_buffer_->clear();
+
+        if (isHeadposeValid(input_data)) {
+            AISDK_LOG_TRACE("[CheckHeadposeCalculator] headpose check ok.");
+            *output_buffer_ = input_data;
+            cc->Outputs().Tag("OUTPUT").Add(output_buffer_.release(), cc->InputTimestamp());
+            AISDK_LOG_TRACE("[CheckHeadposeCalculator] headpose check complete.");
+        } else {
+            AISDK_LOG_TRACE("[CheckHeadposeCalculator] Get invalid headpose input, input truncated here!");
+        }
+
+        AISDK_LOG_TRACE("[CheckHeadposeCalculator] Process complete.");
+        return absl::OkStatus();
+    }
+};
+
+}  // namespace mediapipe
---
+++ aisdk/algorithm/internal_structs/headpose_struct_internal.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <vector>
+
+#include "interface/handtracking_sdk/public/nr_plugin_types.h"
+
+namespace aisdk::algorithm {
+
+struct HeadPoseInternal {
+    HeadPoseInternal() {
+        transform.rotation.qw = 1.0;
+        transform.rotation.qx = 0.0;
+        transform.rotation.qy = 0.0;
+        transform.rotation.qz = 0.0;
+
+        transform.position.x = 0.0;
+        transform.position.y = 0.0;
+        transform.position.z = 0.0;
+    }
+
+    HeadPoseInternal(NRTransform init_transform) : transform(init_transform) {}
+
+    NRTransform transform;
+
+    void clear() {
+        transform.rotation.qw = 1.0;
+        transform.rotation.qx = 0.0;
+        transform.rotation.qy = 0.0;
+        transform.rotation.qz = 0.0;
+
+        transform.position.x = 0.0;
+        transform.position.y = 0.0;
+        transform.position.z = 0.0;
+    }
+};
+
+}  // namespace aisdk::algorithm
