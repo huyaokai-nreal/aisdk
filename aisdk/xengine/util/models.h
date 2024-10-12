dc0d81ff19a584f144280b158b9eb052a9624e16
NrealLzx <zx_li@nreal.ai>
2024-10-12 09:14:29 +0000

    fix: rm unused pipeline config

--- aisdk/xengine/util/models.h
+++
@@ -1,7 +0,0 @@
-#ifndef _MODELS_H_IN_
-#define _MODELS_H_IN_
-
-#define DEFAULT_PIPELINE_TAR_PATH "/aisdk/build_linux/Release/handtracking_pipeline_v2.0.0.tar.enc"
-#define DEFAULT_PIPELINE_TAR_KEY "60 22 32 1A 0F 1B 4B B3 A9 3C 51 5D E4 6D 65 64 B2 2B A6 57 BC 0C 23 84 EC C1 2C CA 1B F8 79 F2 "
-
-#endif  // _MODELS_H_IN_
