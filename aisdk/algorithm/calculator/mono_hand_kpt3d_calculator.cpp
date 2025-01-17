f43e1b51a37db36461d83de9fb6b6f586426cdc0
huyaokai-nreal <ykhu@xreal.com>
2025-01-17 09:19:38 +0000

    fix(mono):resolve hand flickering issue when hand is close to glasses

--- aisdk/algorithm/calculator/mono_hand_kpt3d_calculator.cpp
+++ aisdk/algorithm/calculator/mono_hand_kpt3d_calculator.cpp
@@ -112,7 +112,8 @@ class MonoHandKpt3DCalculator : public xgraph::CalculatorBase {
             if (status.ok()) {
                 output_buffer_->lhand_valid = true;
                 output_buffer_->left_hand.source = CamType::MONO;
-                output_buffer_->left_hand.score = 1.0;
+                AISDK_LOG_TRACE("[MonoHandKpt3DSolver] left_hand root: {} {} {}", output_buffer_->left_hand.kpt3d[0][0],
+                                output_buffer_->left_hand.kpt3d[0][1], output_buffer_->left_hand.kpt3d[0][2]);
                 if (kpt3d_world_pre.left_hand.source == CamType::BINO) {
                     auto kpt3d_cam_pre =
                         recal_lcam_kpt3d_cv_with_new_headpose(headpose_data.transform, kpt3d_world_pre.left_hand.kpt3d);
@@ -129,6 +130,17 @@ class MonoHandKpt3DCalculator : public xgraph::CalculatorBase {
                 } else {
                     output_buffer_->left_hand.kpt2d_lcam = kpt2d.lhand_lcam_kpt;
                 }
+                // 如果手在中间且离眼镜较近，或者手在边缘且离眼镜特别近时，单目无效
+                if ((output_buffer_->left_hand.kpt3d[0][2] <= 0.3 &&
+                     output_buffer_->left_hand.kpt2d_lcam[0][0] >= 100) ||
+                    (output_buffer_->left_hand.kpt3d[0][2] <= 0.1 &&
+                     output_buffer_->left_hand.kpt2d_lcam[0][0] < 100)) {
+                    output_buffer_->left_hand.score = 0.;
+                } else {
+                    output_buffer_->left_hand.score = 1.0;
+                }
+                AISDK_LOG_TRACE("[MonoHandKpt3DSolver] left_hand pixel_x: {}",
+                                output_buffer_->left_hand.kpt2d_lcam[0][0]);
             } else {
                 AISDK_LOG_TRACE("[MonoHandKpt3DSolver] Falied to solve left hand on left image: {}", status.message());
             }
@@ -156,7 +168,10 @@ class MonoHandKpt3DCalculator : public xgraph::CalculatorBase {
             if (status.ok()) {
                 output_buffer_->rhand_valid = true;
                 output_buffer_->right_hand.source = CamType::MONO;
-                output_buffer_->right_hand.score = 1.0;
+
+                AISDK_LOG_TRACE("[MonoHandKpt3DSolver] right_hand root: {} {} {}",
+                                output_buffer_->right_hand.kpt3d[0][0], output_buffer_->right_hand.kpt3d[0][1],
+                                output_buffer_->right_hand.kpt3d[0][2]);
                 if (kpt3d_world_pre.right_hand.source == CamType::BINO) {
                     auto kpt3d_cam_pre = recal_lcam_kpt3d_cv_with_new_headpose(headpose_data.transform,
                                                                                kpt3d_world_pre.right_hand.kpt3d);
@@ -173,6 +188,17 @@ class MonoHandKpt3DCalculator : public xgraph::CalculatorBase {
                 } else {
                     output_buffer_->right_hand.kpt2d_rcam = kpt2d.rhand_rcam_kpt;
                 }
+                // 如果手在中间且离眼镜较近，或者手在边缘且离眼镜特别近时，单目无效
+                if ((output_buffer_->right_hand.kpt3d[0][2] <= 0.3 &&
+                     output_buffer_->right_hand.kpt2d_rcam[0][0] <= 400) ||
+                    (output_buffer_->right_hand.kpt3d[0][2] <= 0.1 &&
+                     output_buffer_->right_hand.kpt2d_rcam[0][0] > 400)) {
+                    output_buffer_->right_hand.score = 0.;
+                } else {
+                    output_buffer_->right_hand.score = 1.0;
+                }
+                AISDK_LOG_TRACE("[MonoHandKpt3DSolver] right_hand pixel_x: {}",
+                                output_buffer_->right_hand.kpt2d_rcam[0][0]);
             } else {
                 AISDK_LOG_TRACE("[MonoHandKpt3DSolver] Falied to solve right  hand on right image: {}",
                                 status.message());
