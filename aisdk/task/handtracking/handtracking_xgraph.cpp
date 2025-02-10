553f4fab375de71c2264a2b97a4e479ca2a69165
shuaiwang-xreal <shuaiwang@xreal.com>
2025-02-10 14:45:21 +0800

    fix(AIK): fix ring finger pinch and thumb bending problem

--- aisdk/task/handtracking/handtracking_xgraph.cpp
+++ aisdk/task/handtracking/handtracking_xgraph.cpp
@@ -337,14 +337,18 @@ aisdk::algorithm::Status HandTrackingXGraph::PopResult(uint64_t hmd_time_nano, u
                     predicted_points[k] = ontracked_points[i][k] + root_kf_predicted - root_meas;
                 }
                 predicted_points = post_filter_->process(i, predicted_points);
-                if ((!hand_data_internal.left_hand.constrained && i == 0) ||
-                    (!hand_data_internal.right_hand.constrained && i == 1)) {
-                    auto points_mano = constraint_hand_v2(predicted_points, (i == 0));
-                    predicted_points = points_mano;
-                } else {
-                    constraint_hand_plane(predicted_points);
-                    constraint_thumb(predicted_points);
-                }
+
+                //对部分关键点进行约束（这里3D和非3D使用同一套逻辑）
+                auto points_mano = constraint_hand_v2(predicted_points, (i == 0));
+                predicted_points = points_mano;
+                // if ((!hand_data_internal.left_hand.constrained && i == 0) ||
+                //     (!hand_data_internal.right_hand.constrained && i == 1)) {
+                //     auto points_mano = constraint_hand_v2(predicted_points, (i == 0));
+                //     predicted_points = points_mano;
+                // } else {
+                //     constraint_hand_plane(predicted_points);
+                //     constraint_thumb(predicted_points);
+                // }
 
 #if defined(ENABLE_OPENXR_HANDJOINT_FORMAT)
                 algorithm::compute_xr_joint_rotation_v1(predicted_points, (i == 0), ontracked_rotations[i]);
