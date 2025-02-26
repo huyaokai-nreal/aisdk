73b79bc424ca7789ce00641da2da5053bb057b6c
shuaiwang-xreal <shuaiwang@xreal.com>
2025-02-26 14:24:16 +0800

    doc: add some comments

--- aisdk/algorithm/calculator/convert_to_world_calculator.cpp
+++ aisdk/algorithm/calculator/convert_to_world_calculator.cpp
@@ -10,12 +10,16 @@
 
 namespace aisdk::algorithm {
 
+/// @brief 将手部关键点3D坐标从相机坐标系转换到世界坐标系
+/// @param headpose 手部关键点的3D坐标
+/// @param points_lcam_cv
+/// @return
 std::vector<Vec3f_t> transfer_from_cvL_to_world(NRTransform headpose, const std::vector<Vec3f_t>& points_lcam_cv) {
     std::vector<Vec3f_t> points_lcam_gl, points_head, points_world;
 
-    TransferCVToGL(points_lcam_cv, points_lcam_gl);
-    TransferLeftCamToHead(points_lcam_gl, points_head);
-    TransferHeadToWorld(headpose, points_head, points_world);
+    TransferCVToGL(points_lcam_cv, points_lcam_gl);  //将openCV坐标系（右-下-前）转换为openGL坐标系（右-上-后）
+    TransferLeftCamToHead(points_lcam_gl, points_head);  //将左相机坐标系转换到头部坐标系
+    TransferHeadToWorld(headpose, points_head, points_world);  //利用头部姿态数据将头部坐标系转换到世界坐标系
 
     return points_world;
 }
@@ -29,18 +33,26 @@ std::vector<Vec3f_t> transfer_from_cvL_to_world(NRTransform headpose, const std:
 //   output_stream: "OUTPUT:kpt3d_world"
 // }
 
+/// @brief
+/// 将手部关键点从相机坐标系转换到世界坐标系
 class ConvertToWorldCalculator : public xgraph::CalculatorBase {
    private:
    public:
+    /// @brief 设置calculator的输入输出关系和对应数据类型
+    /// @param cc mediapipe计算图的上下文（提供输出输出流，SidePacket，选项参数等）
+    /// @return absl::OkStatus()
     static absl::Status GetContract(xgraph::CalculatorContract* cc) {
         AISDK_LOG_TRACE("[ConvertToWorldCalculator] GetContract start.");
-        cc->Inputs().Tag("INPUT").Set<HandsData>();
-        cc->Inputs().Tag("HEADPOSE").Set<HeadPoseInternal>();
+        cc->Inputs().Tag("INPUT").Set<HandsData>();            //输入手部数据
+        cc->Inputs().Tag("HEADPOSE").Set<HeadPoseInternal>();  //输入头部姿态
         cc->Outputs().Tag("OUTPUT").Set<HandsData>();
         AISDK_LOG_TRACE("[ConvertToWorldCalculator] GetContract complete.");
         return absl::OkStatus();
     }
 
+    /// @brief 加载模型，分配资源，初始化参数（计算节点启动时执行一次）
+    /// @param cc mediapipe计算图的上下文（提供输入输出流，SidePacket，选项参数等）
+    /// @return 返回结果，成功返回absl::OkStatus()
     absl::Status Open(xgraph::CalculatorContext* cc) final {
         AISDK_LOG_TRACE("[ConvertToWorldCalculator] Open start.");
         AISDK_LOG_TRACE("[ConvertToWorldCalculator] Open complete.");
@@ -59,18 +71,24 @@ class ConvertToWorldCalculator : public xgraph::CalculatorBase {
 
             std::unique_ptr<HandsData> output_buffer_ = absl::make_unique<HandsData>();
             *output_buffer_ = input_data;
+
+            //处理左手数据
             if (input_data.lhand_valid) {
                 AISDK_LOG_TRACE("[ConvertToWorldCalculator] Transform left hand 3d kpt form cv left to world!");
                 output_buffer_->left_hand.kpt3d =
                     transfer_from_cvL_to_world(headpose_data.transform, input_data.left_hand.kpt3d);
                 AISDK_LOG_TRACE("[ConvertToWorldCalculator] Transform left hand 3d kpt complete!");
             }
+
+            //处理右手数据
             if (input_data.rhand_valid) {
                 AISDK_LOG_TRACE("[ConvertToWorldCalculator] Transform right hand 3d kpt form cv left to world!");
                 output_buffer_->right_hand.kpt3d =
                     transfer_from_cvL_to_world(headpose_data.transform, input_data.right_hand.kpt3d);
                 AISDK_LOG_TRACE("[ConvertToWorldCalculator] Transform right hand 3d kpt complete!");
             }
+
+            //输出
             if (output_buffer_->lhand_valid || output_buffer_->rhand_valid) {
                 cc->Outputs().Tag("OUTPUT").Add(output_buffer_.release(), cc->InputTimestamp());
             } else {
--- aisdk/algorithm/calculator/gesture_recognition_calculator.cpp
+++ aisdk/algorithm/calculator/gesture_recognition_calculator.cpp
@@ -23,30 +23,42 @@ namespace aisdk::algorithm {
 //   output_stream: "GR_OUTPUT:gesture"
 // }
 
+/// @brief
+/// 结合2D/3D手势关键点进行实时手势分类，输出手势结果
 class GestureRecognitionCalculator : public xgraph::CalculatorBase {
    private:
-    std::unique_ptr<GestureRecognitionV2> m_gesture_classifier_lhand;
-    std::unique_ptr<GestureRecognitionV2> m_gesture_classifier_rhand;
+    std::unique_ptr<GestureRecognitionV2> m_gesture_classifier_lhand;  //左手手势分类器
+    std::unique_ptr<GestureRecognitionV2> m_gesture_classifier_rhand;  //右手手势分类器
 
    public:
+    /// @brief 设置calculator的输入输出关系和对应数据类型
+    /// @param cc mediapipe计算图的上下文（提供输出输出流，SidePacket，选项参数等）
+    /// @return absl::OkStatus()
     static absl::Status GetContract(xgraph::CalculatorContract* cc) {
         AISDK_LOG_TRACE("[GestureRecognitionCalculator] GetContract start.");
-        cc->Inputs().Tag("GR_KPT_INPUT").Set<HandsData>();
-        cc->Inputs().Tag("GR_KPT2D_INPUT").Set<Kpt2dInternal>();
-        cc->Outputs().Tag("GR_OUTPUT").Set<HandGestureInternal>();
+        cc->Inputs().Tag("GR_KPT_INPUT").Set<HandsData>();  // 输入：3D手部关键点（相机/世界坐标系）
+        cc->Inputs().Tag("GR_KPT2D_INPUT").Set<Kpt2dInternal>();    // 输入：2D手部关键点（图像坐标系）
+        cc->Outputs().Tag("GR_OUTPUT").Set<HandGestureInternal>();  // 输出：手势识别结果
         AISDK_LOG_TRACE("[GestureRecognitionCalculator] GetContract complete.");
         return absl::OkStatus();
     }
 
+    /// @brief 加载模型，分配资源，初始化参数（计算节点启动时执行一次）
+    /// @param cc mediapipe计算图的上下文（提供输入输出流，SidePacket，选项参数等）
+    /// @return 返回结果，成功返回absl::OkStatus()
     absl::Status Open(xgraph::CalculatorContext* cc) final {
         AISDK_LOG_TRACE("[GestureRecognitionCalculator] Open start.");
-        m_gesture_classifier_lhand = std::make_unique<GestureRecognitionV2>();
-        m_gesture_classifier_rhand = std::make_unique<GestureRecognitionV2>();
-        const auto& options = cc->Options<aisdk::GestureRecognitionCalculatorOptions>();
+        m_gesture_classifier_lhand = std::make_unique<GestureRecognitionV2>();            //初始化左手分类器
+        m_gesture_classifier_rhand = std::make_unique<GestureRecognitionV2>();            //初始化右手分类器
+        const auto& options = cc->Options<aisdk::GestureRecognitionCalculatorOptions>();  //读取配置参数
+
+        //设置捏合阈值
         if (options.pinch_th() > 0) {
             m_gesture_classifier_lhand->set_pinch_close_th(options.pinch_th());
             m_gesture_classifier_rhand->set_pinch_close_th(options.pinch_th());
         }
+
+        //设置捏合宽度阈值
         if (options.pinch_th_width() > 0) {
             m_gesture_classifier_lhand->set_pinch_close_th_width(options.pinch_th_width());
             m_gesture_classifier_rhand->set_pinch_close_th_width(options.pinch_th_width());
@@ -60,13 +72,14 @@ class GestureRecognitionCalculator : public xgraph::CalculatorBase {
         TIMER_ONCE_WITH_TAG(GestureRecognitionCalculator::Process);
 #endif
         AISDK_LOG_TRACE("[GestureRecognitionCalculator] Process start.");
-        const auto& kpt3d_data = cc->Inputs().Tag("GR_KPT_INPUT").Get<HandsData>();
-        const auto& kpt2d_data = cc->Inputs().Tag("GR_KPT2D_INPUT").Get<Kpt2dInternal>();
+        const auto& kpt3d_data = cc->Inputs().Tag("GR_KPT_INPUT").Get<HandsData>();  //当前帧的3D关键点坐标
+        const auto& kpt2d_data = cc->Inputs().Tag("GR_KPT2D_INPUT").Get<Kpt2dInternal>();  //当前帧的2D关键点坐标
 
         std::unique_ptr<HandGestureInternal> output_buffer_ = absl::make_unique<HandGestureInternal>();
-        const auto kpt3d_world_pre = GlobalPredictorService::getInstance().get_last_kpt3d_world();
+        const auto kpt3d_world_pre = GlobalPredictorService::getInstance().get_last_kpt3d_world();  //历史3D关键点坐标
         AISDK_LOG_TRACE("[GestureRecognitionCalculator] Process start 2.");
 
+        //左手手势识别
         if (kpt3d_data.lhand_valid) {
             AISDK_LOG_TRACE("[GestureRecognitionCalculator] process left hand.");
 
@@ -77,6 +90,8 @@ class GestureRecognitionCalculator : public xgraph::CalculatorBase {
             AISDK_LOG_TRACE("[GestureRecognitionCalculator] process left hand complete. {}",
                             HandGestureNames[static_cast<int>(output_buffer_->lhand_gesture)]);
         }
+
+        //右手手势识别
         if (kpt3d_data.rhand_valid) {
             AISDK_LOG_TRACE("[GestureRecognitionCalculator] process right hand.");
             auto [gesture_res, raw_feat] = m_gesture_classifier_rhand->predict_with_keypoints3d(
@@ -87,6 +102,7 @@ class GestureRecognitionCalculator : public xgraph::CalculatorBase {
                             HandGestureNames[static_cast<int>(output_buffer_->rhand_gesture)]);
         }
 
+        //输出
         cc->Outputs().Tag("GR_OUTPUT").Add(output_buffer_.release(), cc->InputTimestamp());
         AISDK_LOG_TRACE("[GestureRecognitionCalculator] Process complete.");
         return absl::OkStatus();
