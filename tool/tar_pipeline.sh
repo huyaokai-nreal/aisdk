24b2f84ba4ed42b175dd14fba9488210f036363a
shuaiwang-xreal <shuaiwang@xreal.com>
2025-05-16 17:54:50 +0800

    style: add some comment

--- tool/tar_pipeline.sh
+++ tool/tar_pipeline.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
+set -e  # 任何命令失败，立即终止脚本
+
 output_dir="${1:-"build"}"
-pp=$(pwd)
 tar_ppls=${2}
 tar_models=${3}
+
+pp=$(pwd)
 aes_tools=${pp}/tool/aes_encrypt_tools
 tar_path=${pp}/config/${tar_ppls}
 tar_out=${output_dir}/handtracking_pipeline_v2.0.0.tar
 model_src_path=${pp}/thirdparty/ai_model_zoo/${tar_models}
+
 # 复制模型文件
 cp -r ${model_src_path} ${tar_path}/models
+
 # 需要进入目录后打包
 cd ${tar_path}
 tar -cvf ${tar_out} *.json *.txt models
 ${aes_tools} ${tar_out} ${tar_out}.enc enc > ${tar_out}.key
+
 # 删除中间文件
 rm ${tar_out}
 rm -r ${tar_path}/models
+
+echo "成功生成加密包: ${tar_out}.enc"
